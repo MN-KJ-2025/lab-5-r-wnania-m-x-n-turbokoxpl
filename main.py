cb92dda30d95390272236d0bdb260e32e538c08f
Krzysztof Olszewski <krzysztofjuzolszewski@gmail.com>
2025-11-23 21:38:10 +0100

    zmiany test

--- main.py
+++ main.py
@@ -24,15 +24,17 @@ def spare_matrix_Abt(m: int, n: int) -> Union[Tuple[np.ndarray, np.ndarray],None
             - Wektor b (m,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
+    if not isinstance(m,int) or not isinstance(n,int):
+        return None
+    if m<=0 or n<=0:
+        return None
     t = np.linspace(0,1,num=m)
     A = np.vander(t,n,increasing=True)
-    b = np.cos(t)
+    b = np.cos(4*t)
     return A,b
 
 
-def square_from_rectan(
-    A: np.ndarray, b: np.ndarray
-) -> Union[Tuple[np.ndarray, np.ndarray],None]:
+def square_from_rectan(A: np.ndarray, b: np.ndarray) -> Union[Tuple[np.ndarray, np.ndarray],None]:
     """Funkcja przekształcająca układ równań z prostokątną macierzą współczynników
     na kwadratowy układ równań.
     A^T * A * x = A^T * b  ->  A_new * x = b_new
@@ -47,6 +49,18 @@ def square_from_rectan(
             - Wektor b_new (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
+    if not isinstance(A, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    if A.ndim !=2:
+        return None
+    m, n = A.shape
+    if m<=0 or n<=0:
+        return None
+    if b.ndim != 1:
+        return None
+    if b.shape[0] != m:
+        return None
+    
     A_new= A.T @ A
     b_new=A.T @ b
     
@@ -66,5 +80,12 @@ def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> Union[float,No
         (float): Wartość normy residuum dla podanych parametrów.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
+    if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    if A.ndim != 2 or x.ndim != 1 or b.ndim != 1:
+        return None
     
+    m, n = A.shape
+    if x.shape[0] != n or b.shape[0] != m:
+        return None
     return np.linalg.norm(A @x-b)
